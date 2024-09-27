3d6767928142373afa089e3c542483a3af89ec67
sakir456 <shaikhsakir1015@gmail.com>
2024-09-28 01:09:09 +0530

    first commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,43 @@
+import { assets } from "../assets/assets"
+
+
+const Footer = () => {
+  return (
+    <div className="md:mx-10">
+       <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
+          {/* ----left section----- */}
+          <div>
+            <img className="mb-5 w-40 " src={assets.logo} alt=""/> 
+            <p className="w-full md:w-2/3 text-gray-600 leading-6">For Technical queries and post sales: support@docpulse.com For Sales, Please fill the form below. Our Sales team will get back to you shortly.</p>
+          </div>
+
+          {/* ----Center section----- */}
+          <div>
+             <p className="text-xl font-medium mb-5 ">COMPANY</p>
+             <ul className="flex flex-col gap-2 text-gray-600">
+                <li>Home</li>
+                <li>About us</li>
+                <li>Contact us</li>
+                <li>Privacy Policy</li>
+             </ul>
+          </div>
+
+          {/* ----Right section----- */}
+          <div >
+             <p className="text-xl font-medium mb-5 ">GET IN TOUCH</p>
+             <ul className="flex flex-col gap-2 text-gray-600">
+                <li>+1-212-456-789</li>
+                <li>sakir@gmail.com</li>
+             </ul>
+          </div>
+       </div>
+       {/*----- CopyRight Text----- */}
+       <div>
+        <hr/>
+        <p className="py-5 text-sm text-center">CopyRight 2024@ Prescripto -All Right Reserved. </p>
+       </div>
+    </div>
+  )
+}
+
+export default Footer
