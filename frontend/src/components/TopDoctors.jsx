2ed5f45a264cd3143e183a32ee2043dc34ab57ae
sakir456 <shaikhsakir1015@gmail.com>
2024-10-02 00:03:46 +0530

    updated

--- frontend/src/components/TopDoctors.jsx
+++ frontend/src/components/TopDoctors.jsx
@@ -14,7 +14,7 @@ const {doctors} = useContext(AppContext)
        <p className="sm:w-1/3 text-center text-sm">Simply Browse through our extensive list of trusted doctors.</p>   
        <div className="w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0">
           {doctors.slice(0,10).map((item,index)=>(
-           <div onClick={()=>navigate(`/appointment/${item._id}`)} className="border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500" key={index} >
+           <div onClick={()=>{navigate(`/appointment/${item._id}`); scrollTo(0,0)}} className="border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500" key={index} >
             <img 
             className="bg-blue-50" src={item.image} alt=""/>
             <div className="p-4">
