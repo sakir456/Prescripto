8497e811fb66010089a3e206c6e1e57c1dc3de5b
sakir456 <shaikhsakir1015@gmail.com>
2024-10-03 22:37:37 +0530

    Frontend completed

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -16,10 +16,14 @@ const Login = () => {
     <div className="flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-zinc-600 text-sm shadow-lg">
       <p className="text-2xl font-semibold">{state === "Sign Up" ? "Create Acoount" : "Login"}</p>
       <p>Please {state === "Sign Up" ? "sign up" : "log in"}  to book appointment</p>
-      <div className="w-full">
-        <p>Full Name</p>
+      {
+        state === "Sign Up" &&  <div className="w-full">
+           <p>Full Name</p>
         <input className="border border-zinc-300 rounded w-full p-2 mt-1" type="text" onChange={(e)=>setName(e.target.name)} value={name} />
       </div>
+        
+      }
+      
       <div className="w-full">
         <p>Email</p>
         <input className="border border-zinc-300 rounded w-full p-2 mt-1" type="email" onChange={(e)=>setEmail(e.target.name)} value={email} />
@@ -29,7 +33,12 @@ const Login = () => {
         <input className="border border-zinc-300 rounded w-full p-2 mt-1" type="password" onChange={(e)=>setPassword(e.target.name)} value={password} />
       </div>
       <button className="bg-primary text-white w-full py-2 rounded-md text-base">{state === "Sign Up" ? "Create Acoount" : "Login"}</button>
-      
+      {
+        state ==="Sign Up" ?
+        <p>Already have an account?<span  onClick={()=> setState("Login")} className="text-primary underline cursor-pointer">Login here</span>  </p> :
+        <p>Create a new account ?<span onClick={()=> setState("Sign Up")} className="text-primary underline cursor-pointer">click here</span> </p>
+
+      }
     </div>
       
     </form>
