826abe88af74fe3719a79e365c71ace74ef8a272
sakir456 <shaikhsakir1015@gmail.com>
2024-10-24 23:47:53 +0530

    updated

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,13 +1,22 @@
 import { NavLink, useNavigate } from "react-router-dom"
 import {assets} from "../assets/assets"
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { AppContext } from "../context/AppContext";
+
 
 const Navbar = () => {
 
     const navigate = useNavigate();
 
+    const {token, setToken} = useContext(AppContext)
+
     const [showMenu, setShowMenu] = useState(false)
-    const [token, setToken] = useState(true)
+
+    const logout = () => {
+        setToken(false)
+        localStorage.removeItem("token")
+    }
+    
   return (
     <div className="flex items-center justify-between text-sm py-3 mb-5 border-b border-b-gray-400">
         <img onClick={()=>navigate("/")} className="w-44 cursor-pointer" src={assets.logo} alt="" />
@@ -38,7 +47,7 @@ const Navbar = () => {
             <div className="min-w-48 bg-stone-100 rounded flex flex-col gap-3 p-4 ">
                 <p onClick={() => navigate("my-profile")} className="hover:text-black cursor-pointer">My Profile</p>
                 <p onClick={() => navigate("my-appointments")} className="hover:text-black cursor-pointer">My Appointments</p>
-                <p onClick={() => setToken(false)} className="hover:text-black cursor-pointer">Logout</p>
+                <p onClick={logout} className="hover:text-black cursor-pointer">Logout</p>
             </div>
         </div>
          </div> 
