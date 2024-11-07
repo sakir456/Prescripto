0458acfc34142b50b81418804a76bf1bfd706639
sakir456 <shaikhsakir1015@gmail.com>
2024-11-08 00:02:01 +0530

    updated

--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -1,8 +1,8 @@
 import { useContext, useState } from "react"
-import {assets} from "../assets/assets"
 import { AdminContext } from "../context/AdminContext"
 import axios from "axios"
 import { toast } from "react-toastify"
+import { DoctorContext } from "../context/DoctorContext"
 
 const Login = () => {
 
@@ -11,6 +11,7 @@ const Login = () => {
      const [password,setPassword] = useState("")
 
      const {setAToken,backendUrl} = useContext(AdminContext)
+     const {setDToken} = useContext(DoctorContext)
 
      const onSubmitHandler = async (event) => {
           event.preventDefault()
@@ -28,7 +29,18 @@ const Login = () => {
             }
               
             } else {
+               
+              const {data} = await axios.post(backendUrl + "/api/doctor/login", {email, password})
+              if (data.success) {
+                localStorage.setItem("dToken", data.token)
+                setDToken(data.token)
+                console.log(data.token);
+                
+            } else {
+                toast.error(data.message)
+            }
 
+              
             }
 
           } catch (error) {
