a55c0c960390893088af551fb9372bd7785e3f1b
sakir456 <shaikhsakir1015@gmail.com>
2025-03-09 15:22:24 +0530

    Add instructional text for admin and doctor login credentials in the Login component

--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -50,7 +50,9 @@ const Login = () => {
     loading ? (
       <LoadingSpinner />
     ) : (
-    <form onSubmit={onSubmitHandler} className="min-h-[80vh] flex items-center">
+    <form onSubmit={onSubmitHandler} className="min-h-[80vh] flex flex-col items-center">
+     <p className="text-center">Admin Email:admin@prescripto.com / qwerty123</p>
+     <p className="text-center">Doctor Email: jamesmartinez@prescripto.com / 12345678</p>
       <div className="flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-[#5E5E5E] text-sm shadow-lg">
         <p className="text-2xl font-semibold m-auto"><span className="text-primary"> {state} </span> Login</p>
         <div className="w-full">
